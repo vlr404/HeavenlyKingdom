7f82291c1b10b12a2241f386a66a0ad7b1d6ae90
vlr404 <valeratitov443@gmail.com>
2026-03-15 19:51:03 +0200

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/HeavenlyKing/',
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
+      
     }),
   ],
 })
