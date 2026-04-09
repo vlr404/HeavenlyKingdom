e365025bd1e92c21b6ef81c13a07734f9550615b
hellomumu <koleabumaghin@gmail.com>
2026-04-09 12:15:26 +0300

    feat: update package dependencies and simplify App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-
-import './App.css'
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
-import Home from './pages/Home'
-import Shop from './pages/Shop'
-import { Header } from './components/common/Header/Header'
-import Cart from './pages/Cart'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+// import Shop from './pages/Shop'
 
 function App() {
   return (
-    <Router basename="/HeavenlyKing">
-      <div className="App">
-        <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/shop" element={<Shop />} /> */}
+    </Routes>
   )
 }
-
 
 export default App
